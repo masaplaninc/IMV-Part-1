7caefd9ebe2d90486aa5c4c11198fc4423055c23
Vadim Petrov <vadim.vooku.petrov@gmail.com>
2017-11-13 12:28:26 +0100

    Changed to random color

--- js/core.js
+++ js/core.js
@@ -14,14 +14,7 @@ var pMatrix = mat4.create();
 // color array
 // var black = [0.0, 0.0, 0.0, 1.0];
 var red = [1.0, 0.0, 0.2, 1.0];
-var blue = [0.0, 0.2, 1.0, 1.0];
-var green = [0.0, 1.0, 0.2, 1.0];
-var white = [1.0, 1.0, 1.0, 1.0];
-
-var colorArray = [red, blue, green, white];
-var colorIndex = 0;
-var colors = colorArray[colorIndex].concat(colorArray[colorIndex]).concat(colorArray[colorIndex]);
-
+var color = red;
 
 var midiFile;
 
@@ -186,10 +179,10 @@ function initBuffers() {
     triangleVertexColorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexColorBuffer);
 
-    colors = colorArray[colorIndex].concat(colorArray[colorIndex]).concat(colorArray[colorIndex]);
+    
 
     // Pass the colors into WebGL
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.DYNAMIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(color.concat(color).concat(color).concat(color)), gl.DYNAMIC_DRAW);
     triangleVertexColorBuffer.itemSize = 4;
     triangleVertexColorBuffer.numItems = 3;
 
@@ -276,15 +269,11 @@ function start() {
 }
 
 function animate() {
-    colors = colorArray[colorIndex].concat(colorArray[colorIndex]).concat(colorArray[colorIndex]);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.DYNAMIC_DRAW);
-
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(color.concat(color).concat(color).concat(color)), gl.DYNAMIC_DRAW);
 }
 
 function checkPressedKey(e) {
-    colorIndex = (colorIndex + 1) % colorArray.length;
-    
-    console.log(colorIndex);
+    color = [Math.random(), Math.random(), Math.random(), 1.0];
 }
 
 
